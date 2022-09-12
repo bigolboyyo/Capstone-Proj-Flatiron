263aa435c3384108cf2c58a6be0ffb4a45298c65
bigolboyyo <wesley.grant94@gmail.com>
2022-09-12 18:37:38 -0400

    pre-skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// PRE-SKELETON BRANCH BEGINNING...
 
 function App() {
   const [count, setCount] = useState(0);
