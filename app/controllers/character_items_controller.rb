73e7017d876016e26b0cd0e45fb137a46359f8b2
bigolboyyo <wesley.grant94@gmail.com>
2022-09-20 19:19:45 -0400

    Items Controllers

--- app/controllers/character_items_controller.rb
+++ app/controllers/character_items_controller.rb
@@ -1,2 +1,38 @@
 class CharacterItemsController < ApplicationController
+  before_action :find_character_item, only: [:show, :destroy, :update]
+
+  def index
+    render json: CharacterItem.all, adapter: nil,
+           except: creation_ref, status: :ok
+  end
+
+  def show
+    render json: @character_item, status: :ok
+  end
+
+  def create
+    @character_item = character_item.create!(character_item_params)
+    render json: @character_item, status: :created
+  end
+
+  def update
+    @character_item.update!(character_item_params)
+    render json: @character_item, status: :ok
+  end
+
+  def destroy
+    @character_item.destroy
+    head :no_content
+  end
+
+  private
+
+  def character_item_params
+    params.permit(:character_id, :item_id)
+  end
+
+  def find_character_item
+    @character_item = character_item.find(params[:id])
+    !@character_item ? record_not_found(@character_item) : @character_item
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,38 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :destroy, :update]
+
+  def index
+    render json: Item.all, adapter: nil,
+           except: creation_ref, status: :ok
+  end
+
+  def show
+    render json: @item, status: :ok
+  end
+
+  def create
+    @item = item.create!(item_params)
+    render json: @item, status: :created
+  end
+
+  def update
+    @item.update!(item_params)
+    render json: @item, status: :ok
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:item_name)
+  end
+
+  def find_item
+    @item = item.find(params[:id])
+    !@item ? record_not_found(@item) : @item
+  end
 end
