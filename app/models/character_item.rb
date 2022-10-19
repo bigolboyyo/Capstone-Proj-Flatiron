fa18d517fdf738b30fd9d093fca6acba872e39f7
bigolboyyo <wesley.grant94@gmail.com>
2022-10-19 14:33:09 -0400

    moving to css branch

--- app/models/character_item.rb
+++ app/models/character_item.rb
@@ -1,4 +1,7 @@
 class CharacterItem < ApplicationRecord
   belongs_to :character
   belongs_to :item
+
+  # Validates specific item uniqueness to character using active record
+
 end
--- client/src/components/AuthPage/Login.js
+++ client/src/components/AuthPage/Login.js
@@ -23,75 +23,80 @@ function Login({ onLogin, errorMessage, setAuth }) {
 
   return (
     <>
-      <Navbar fixed="top">
+      <Navbar className="navbar" fixed="top">
         <Container fluid>
           <h1 className="nav-title">Robot Text Adventure</h1>
         </Container>
       </Navbar>
 
-      <div className="form-div">
-        Login
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            onLogin(userCreds);
-            setUserCreds({ username: "", password: "" });
-          }}
-        >
-          <Form.Group
-            as={Row}
-            className="mb-3"
-            controlId="formHorizontalUserName"
+      <Container className="form-container">
+        <div className="form-div">
+          Login
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              onLogin(userCreds);
+              setUserCreds({ username: "", password: "" });
+            }}
           >
-            <Form.Label column sm={{ span: 20 }}>
-              Username:
-            </Form.Label>
-            <Col sm={{ span: 20 }}>
-              <Form.Control
-                name="username"
-                type="text"
-                placeholder="Enter username"
-                value={userCreds.username}
-                onChange={handleChange}
-                required={true}
-              />
-            </Col>
-            {/* <Form.Text className="text-muted">Username must be unique</Form.Text> */}
-          </Form.Group>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formHorizontalUserName"
+            >
+              <Form.Label column sm={{ span: 20 }}>
+                Username:
+              </Form.Label>
+              <Col sm={{ span: 20 }}>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  placeholder="Enter username"
+                  value={userCreds.username}
+                  onChange={handleChange}
+                  required={true}
+                />
+              </Col>
+              {/* <Form.Text className="text-muted">Username must be unique</Form.Text> */}
+            </Form.Group>
 
-          <Form.Group as={Row} className="mb-3" controlId="formBasicPassword">
-            <Form.Label column sm={{ span: 20 }}>
-              Password:
-            </Form.Label>
-            <Col sm={{ span: 20 }}>
-              <Form.Control
-                name="password"
-                type="password"
-                placeholder="Enter Password"
-                value={userCreds.password}
-                onChange={handleChange}
-                required={true}
-              />
+            <Form.Group as={Row} className="mb-3" controlId="formBasicPassword">
+              <Form.Label column sm={{ span: 20 }}>
+                Password:
+              </Form.Label>
+              <Col sm={{ span: 20 }}>
+                <Form.Control
+                  name="password"
+                  type="password"
+                  placeholder="Enter Password"
+                  value={userCreds.password}
+                  onChange={handleChange}
+                  required={true}
+                />
+              </Col>
+            </Form.Group>
+            <Col sm={{ span: 10, offset: 5 }}>
+              <button type="submit">Submit</button>
             </Col>
-          </Form.Group>
-          <Col sm={{ span: 10, offset: 5 }}>
-            <button type="submit">Submit</button>
-          </Col>
-        </Form>
-        <Nav>
-          <Row>
-            <Col sm={{ span: 20, offset: 1 }}>
-              <Nav.Item className="auth-item">
-                Need an account?
-                <Nav.Link className="auth-link" onClick={() => setAuth(false)}>
-                  SignUp
-                </Nav.Link>
-              </Nav.Item>
-            </Col>
-          </Row>
-        </Nav>
-        {errorMessage()}
-      </div>
+          </Form>
+          <Nav>
+            <Row>
+              <Col sm={{ span: 20, offset: 1 }}>
+                <Nav.Item className="auth-item">
+                  Need an account?
+                  <Nav.Link
+                    className="auth-link"
+                    onClick={() => setAuth(false)}
+                  >
+                    SignUp
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+          </Nav>
+          {errorMessage()}
+        </div>
+      </Container>
     </>
   );
 }
